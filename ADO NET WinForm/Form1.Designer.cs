7d28368b94b9df6459510276a10a3d5a788c2384
Dimon <Dimon@DIMON>
2021-11-06 20:20:01 +0400

    Add project files.

---
+++ ADO NET WinForm/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace ADO_NET_WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.comboBoxTask8 = new System.Windows.Forms.ComboBox();
+            this.comboBoxTask9 = new System.Windows.Forms.ComboBox();
+            this.comboBoxTask10 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(325, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(463, 385);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Task1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Task2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(175, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 34);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Task3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(13, 63);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 34);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Task4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(94, 63);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 34);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Task5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(175, 63);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 34);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Task6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(13, 113);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 34);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Task7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 153);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 34);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Task8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(12, 193);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 34);
+            this.button9.TabIndex = 7;
+            this.button9.Text = "Task9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(13, 233);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 34);
+            this.button10.TabIndex = 12;
+            this.button10.Text = "Task10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(94, 113);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 34);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "Task11";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(175, 113);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 34);
+            this.button12.TabIndex = 10;
+            this.button12.Text = "Task12";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // comboBoxTask8
+            // 
+            this.comboBoxTask8.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTask8.FormattingEnabled = true;
+            this.comboBoxTask8.Location = new System.Drawing.Point(94, 161);
+            this.comboBoxTask8.Name = "comboBoxTask8";
+            this.comboBoxTask8.Size = new System.Drawing.Size(156, 21);
+            this.comboBoxTask8.TabIndex = 13;
+            // 
+            // comboBoxTask9
+            // 
+            this.comboBoxTask9.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTask9.FormattingEnabled = true;
+            this.comboBoxTask9.Location = new System.Drawing.Point(94, 201);
+            this.comboBoxTask9.Name = "comboBoxTask9";
+            this.comboBoxTask9.Size = new System.Drawing.Size(156, 21);
+            this.comboBoxTask9.TabIndex = 14;
+            // 
+            // comboBoxTask10
+            // 
+            this.comboBoxTask10.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTask10.FormattingEnabled = true;
+            this.comboBoxTask10.Location = new System.Drawing.Point(94, 241);
+            this.comboBoxTask10.Name = "comboBoxTask10";
+            this.comboBoxTask10.Size = new System.Drawing.Size(156, 21);
+            this.comboBoxTask10.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxTask10);
+            this.Controls.Add(this.comboBoxTask9);
+            this.Controls.Add(this.comboBoxTask8);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.ComboBox comboBoxTask8;
+        private System.Windows.Forms.ComboBox comboBoxTask9;
+        private System.Windows.Forms.ComboBox comboBoxTask10;
+    }
+}
+
---
+++ ADO NET WinForm/SQLQuery1.sql
@@ -0,0 +1,116 @@
+﻿USE [Stationery Firm]
+
+CREATE TABLE TypeStationery(
+[Id] int Primary Key IDENTITY(1,1),
+[Type] nvarchar(100) NOT NULL UNIQUE,
+[Quantity] int not null --Количество канцтоваров каждого типа
+)
+
+CREATE TABLE Stationery(
+[Id] int Primary Key IDENTITY(1,1),
+[Name] nvarchar(100) NOT NULL,
+[TypeStationeryId] int NOT NULL FOREIGN KEY REFERENCES TypeStationery(Id),
+[CostPrice] money not NULL,
+)
+
+CREATE TABLE Managers(
+[Id] int Primary Key IDENTITY(1,1),
+[Name] nvarchar(100) NOT NULL,
+)
+
+CREATE TABLE Customers(
+[Id] int Primary Key IDENTITY(1,1),
+[Name] nvarchar(100) NOT NULL
+)
+
+CREATE TABLE Sales(
+[Id] int Primary Key IDENTITY(1,1),
+[ManagerId] int NOT NULL FOREIGN KEY REFERENCES Managers(Id),
+[CustomerId] int NOT NULL FOREIGN KEY REFERENCES Customers(Id),
+[StationaryId] int NOT NULL FOREIGN KEY REFERENCES Stationery(Id),
+[Price_of_one] money NOT NULL,
+[Quantity] int NOT NULL,
+[Date_of_sale] DATETIME NOT NULL Default(GETDATE())
+)
+
+--INSERT INTO TypeStationery(Type, Quantity)
+--VALUES (N'Pen', 444)
+
+--INSERT INTO Stationery(Name, TypeStationeryId, CostPrice)
+--VALUES (N'Gucci', 1, 23.99)
+
+--INSERT INTO Stationery(Name, TypeStationeryId, CostPrice)
+--VALUES (N'Cyber', 1, 15)
+
+--INSERT INTO Managers(Name)
+--VALUES (N'Mamed')
+
+--INSERT INTO Managers(Name)
+--VALUES (N'Maxmud')
+
+--INSERT INTO Customers(Name)
+--VALUES (N'User Company official')
+
+--INSERT INTO Customers(Name)
+--VALUES (N'User Company')
+
+--INSERT INTO Sales(ManagerId, CustomerId, StationaryId, Quantity, Price_of_one, Date_of_sale)
+--VALUES (1, 1, 1, 30, 24, GETDATE())
+
+--INSERT INTO Sales(ManagerId, CustomerId, StationaryId, Quantity, Price_of_one, Date_of_sale)
+--VALUES (1, 1, 1, 45, 30, GETDATE())
+
+--Task 1
+SELECT Stationery.Name, TypeStationery.Type, Stationery.CostPrice FROM Stationery, TypeStationery
+WHERE Stationery.TypeStationeryId = TypeStationery.Id
+
+--Task 2
+SELECT TypeStationery.Type[Type of Stationary] FROM TypeStationery
+
+--Task 3
+SELECT Managers.Name FROM Managers
+
+--Task 4
+SELECT Stationery.Name, TypeStationery.Type, Stationery.CostPrice FROM Stationery, TypeStationery
+WHERE Stationery.TypeStationeryId = TypeStationery.Id
+AND TypeStationery.Quantity = (SELECT MAX(Quantity) FROM TypeStationery)
+
+--Task 5
+SELECT Stationery.Name, TypeStationery.Type, Stationery.CostPrice FROM Stationery, TypeStationery
+WHERE Stationery.TypeStationeryId = TypeStationery.Id
+AND TypeStationery.Quantity = (SELECT MIN(Quantity) FROM TypeStationery)
+
+--Task 6
+SELECT Stationery.Name, Stationery.CostPrice FROM Stationery
+WHERE Stationery.CostPrice = (SELECT MIN(CostPrice) FROM Stationery)
+
+--Task 7
+SELECT Stationery.Name, Stationery.CostPrice FROM Stationery
+WHERE Stationery.CostPrice = (SELECT MAX(CostPrice) FROM Stationery)
+
+--Task 8
+SELECT Stationery.Name, TypeStationery.Type, Stationery.CostPrice FROM Stationery, TypeStationery
+WHERE TypeStationery.Type = 'Pen' AND Stationery.TypeStationeryId = TypeStationery.Id
+
+--Task 9
+SELECT Managers.Name, Stationery.Name, TypeStationery.Type, Sales.Price_of_one, Sales.Quantity, Sales.Date_of_sale FROM Managers, Sales, Stationery, TypeStationery
+WHERE Managers.Name = 'Mamed' 
+AND Sales.ManagerId = Managers.Id AND Sales.StationaryId = Stationery.Id AND Stationery.TypeStationeryId = TypeStationery.Id
+
+--Task 10
+SELECT Customers.Name, Stationery.Name, TypeStationery.Type, Sales.Price_of_one, Sales.Quantity, Sales.Date_of_sale FROM Customers, Sales, Stationery, TypeStationery
+WHERE Customers.Name = 'User Company official' 
+AND Sales.CustomerId = Customers.Id AND Sales.StationaryId = Stationery.Id AND Stationery.TypeStationeryId = TypeStationery.Id
+
+--Task 11
+SELECT TOP 1 Managers.Name, Customers.Name, Sales.Price_of_one, Sales.Quantity, Stationery.Name, TypeStationery.Type, Sales.Date_of_sale FROM Sales, Managers, Customers, Stationery, TypeStationery
+WHERE Sales.CustomerId = Customers.Id AND Sales.ManagerId = Managers.Id AND Sales.StationaryId = Stationery.Id AND Stationery.TypeStationeryId = TypeStationery.Id
+ORDER BY Sales.Date_of_sale DESC
+
+--Task 12
+SELECT TypeStationery.Type, (AVG(TypeStationery.Quantity) / COUNT(TypeStationery.Type))[Average_amount] FROM TypeStationery, Stationery
+WHERE Stationery.TypeStationeryId = TypeStationery.Id
+GROUP BY TypeStationery.Type
+
+SELECT Customers.Name FROM Customers
+
